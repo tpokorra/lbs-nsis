2807aa594469a2eb13dd7d4f89be23220eee55da
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-09-28 22:02:17 +0200

    attempt to build on Fedora24

--- nsis/setup.sh
+++ nsis/setup.sh
@@ -2,4 +2,9 @@
 
 # see also https://cwiki.apache.org/confluence/display/DIRxSBOX/Installing+NSIS+-+All+platforms
 
-yum -y install glibc-devel.i686 libstdc++-devel.i686 glibc-devel libstdc++-devel
+# only do this on CentOS, not on Fedora
+OS=`cat /etc/redhat-release | awk '{print $1}'`
+if [[ "$OS" == "CentOS" ]
+then 
+  yum -y install glibc-devel.i686 libstdc++-devel.i686 glibc-devel libstdc++-devel
+fi
