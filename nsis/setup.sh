6bb1c3c3d3e37344349ce08e119b87fae2e2a9a9
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-09-29 04:25:38 +0200

    fix typo

--- nsis/setup.sh
+++ nsis/setup.sh
@@ -4,7 +4,7 @@
 
 # only do this on CentOS, not on Fedora
 OS=`cat /etc/redhat-release | awk '{print $1}'`
-if [[ "$OS" == "CentOS" ]
+if [[ "$OS" == "CentOS" ]]
 then 
   yum -y install glibc-devel.i686 libstdc++-devel.i686 glibc-devel libstdc++-devel
 fi
